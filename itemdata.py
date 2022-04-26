934dcaedbaa74b0c7e0c77890dd5dde43889e3e9
Ariescyn <68882322+Ariescyn@users.noreply.github.com>
2022-04-26 10:13:53 -0400

    Update itemdata.py

--- itemdata.py
+++ itemdata.py
@@ -1,29 +1,45 @@
 
 item_dict = {
     'Larval Tear':[249,31,0,176],
-    'Somber stone [1]': [176,39],
-#    'somber 2': [177,39],
-#    'somber 3': [178,39],
-    'Somber stone [4]': [179,39],
-#    'somber 5': [180,39],
-#    'somber 6': [181,39],
-#    'somber 7': [182,39],
-    'Somber stone [8]': [183,39],
-#    'somber 9': [184,39],
-    'Somber stone [10]': [185,39],
+    'Ancient Dragon Stone': [156,39],
+    'Somber Ancient Dragon Stone' : [184,39],
+    'Rune Arc': [190,0],
+    'Lords Rune': [103,11],
+
+
+    'Smithing Stone [1]': [116,39],
+    'Smithing Stone [2]': [117,39],
+    'Smithing Stone [3]': [118,39],
+    'Smithing Stone [4]': [119,39],
+    'Smithing Stone [5]': [120,39],
+    'Smithing Stone [6]': [121,39],
+    'Smithing Stone [7]': [122,39],
+    'Smithing Stone [8]': [123,39],
+    'Smithing Stone [9]': [124,39],
+    'Smithing Stone [10]': [125,39],
+
+
+    'Somber Stone [1]': [176,39],
+    'Somber Stone [2]': [177,39],
+    'Somber Stone [3]': [178,39],
+    'Somber Stone [4]': [179,39],
+    'Somber Stone [5]': [180,39],
+    'Somber Stone [6]': [181,39],
+    'Somber Stone [7]': [182,39],
+    'Somber Stone [8]': [183,39],
+    'Somber Stone [9]': [184,39],
+    'Somber Stone [10]': [185,39],
 
     'Golden Rune [1]' : [84,11],
-#    'golden rune 2' : [85,11],
-#    'golden rune 3' : [86,11],
+    'Golden Rune [2]' : [85,11],
+    'Golden Rune [3]' : [86,11],
     'Golden Rune [4]' : [87,11],
-#    'golden rune 5' : [88,11],
-#    'golden rune 6' : [89,11],
-#    'golden rune 7' : [90,11],
+    'Golden Rune [5]' : [88,11],
+    'Golden Rune [6]' : [89,11],
+    'Golden Rune [7]' : [90,11],
     'Golden Rune [8]' : [91,11],
-#    'golden rune 9' : [92,11],
-    'Golden Rune [10]' : [93,11],
-    'Ancient Dragon Stone': [156,39],
-    'Somber Ancient Dragon Stone' : [184,39]
+    'Golden Rune [9]' : [92,11],
+    'Golden Rune [10]' : [93,11]
 
         }
 
